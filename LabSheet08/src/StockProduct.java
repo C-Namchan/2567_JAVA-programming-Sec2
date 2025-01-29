14a2d31c29d68dd6c0aadab09edd30cd2e69af7a
C-Namchan <twilightgamer2555@gmail.com>
2025-01-29 11:23:42 +0700

    Practice 8

---
+++ LabSheet08/src/StockProduct.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class StockProduct {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Input product Id	: ");
+        Product[] products = new Product[4];
+        
+        for(int i =0; i <Product.Length; i++) {
+        	
+        }
+    }
+}
